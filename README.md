25b23f89038070cc2426fed7cd2d8f738ca91957
Anton Riabov <Anton_Riabov@epam.com>
2020-03-27 18:29:08 +0300

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# ts-hello-world
+
+npm i typescript
+tsc
+node server/server.js
+curl 127.0.0.1:3000
---
+++ server/server.ts
@@ -0,0 +1,13 @@
+import express = require('express');
+var os = require('os');
+
+// Create a new express app instance
+const app: express.Application = express();
+
+app.get('/' , function (req, res) {
+  res.send('Hello World! I am "' + os.hostname() + '".');
+});
+
+app.listen(3000, function () {
+  console.log('app is listening on port 3000!');
+});
